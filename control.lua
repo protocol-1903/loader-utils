b7e672d6f5b0103637a034c764797657dd50b533
protocol_1903 <67478786+protocol-1903@users.noreply.github.com>
2026-02-12 09:49:41 -0700

    fixed blueprints and copy/paste not working

--- control.lua
+++ control.lua
@@ -276,7 +276,7 @@ script.on_event(defines.events.on_player_setup_blueprint, function (event)
   -- update entities
   for _, entity in pairs(entities) do
     if base_loaders[entity.name] then
-      local changed = true
+      changed = true
       local tags = entity.tags or {}
       tags["loader-utils"] = tags["loader-utils"] or loader_ids[entity.name]
       entity.tags = tags
