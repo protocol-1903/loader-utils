3645e301a986063baf934f1554fd3f3bc19d3520
protocol_1903 <67478786+protocol-1903@users.noreply.github.com>
2026-03-05 00:56:36 -0700

    temporary settings fixes (only works locally with aai)

---
+++ settings-final-fixes.lua
@@ -0,0 +1,28 @@
+-- disable snapping
+
+if mods["aai-loaders"] then
+  data.raw["string-setting"]["aai-loaders-belt-stacking-mode"].hidden = true
+  data.raw["string-setting"]["aai-loaders-belt-stacking-mode"].forced_value = "off"
+  data.raw["string-setting"]["aai-loaders-belt-stacking-mode"].allowed_values = {"off"}
+  data.raw["bool-setting"]["aai-loaders-enable-snapping"].hidden = true
+  data.raw["bool-setting"]["aai-loaders-enable-snapping"].forced_value = false
+end
+
+if mods["deadlock-beltboxes-loaders"] then
+  data.raw["bool-setting"]["deadlock-loaders-snap-to-belts"].hidden = true
+  data.raw["bool-setting"]["deadlock-loaders-snap-to-belts"].forced_value = false
+  data.raw["bool-setting"]["deadlock-loaders-snap-to-inventories"].hidden = true
+  data.raw["bool-setting"]["deadlock-loaders-snap-to-inventories"].forced_value = false
+end
+
+if mods["comfortable-loader"] then
+  data.raw["bool-setting"]["comfortable-loader-snap-to-belts"].hidden = true
+  data.raw["bool-setting"]["comfortable-loader-snap-to-belts"].forced_value = false
+  data.raw["bool-setting"]["comfortable-loader-snap-to-inventories"].hidden = true
+  data.raw["bool-setting"]["comfortable-loader-snap-to-inventories"].forced_value = false
+end
+
+if mods["vanilla-loaders-hd"] then
+  data.raw["bool-setting"]["vanillaLoaders-do-loader-snapping"].hidden = true
+  data.raw["bool-setting"]["vanillaLoaders-do-loader-snapping"].forced_value = false
+end
